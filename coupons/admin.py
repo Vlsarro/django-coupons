b4cbed301d64e5dafc72c1a195e7ca9781c7cb8b
Vlsarro <drekmaro@gmail.com>
2025-10-18 04:30:07 +0300

    fix retrieving unused coupons, apply https://github.com/byteweaver/django-coupons/pull/35

--- coupons/admin.py
+++ coupons/admin.py
@@ -98,7 +98,7 @@ class CampaignAdmin(admin.ModelAdmin):
     num_coupons_used.short_description = _("used")
 
     def num_coupons_unused(self, obj):
-        return obj.coupons.used().count()
+        return obj.coupons.unused().count()
 
     num_coupons_unused.short_description = _("unused")
 
